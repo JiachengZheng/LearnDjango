5fa8e8cf233d7c2c8767d9a147ed54ba38944393
郑嘉成 <jiacheng_zheng@163.com>
2018-01-03 14:20:02 +0800

    Update wsgi.py

--- apiproject/apiproject/wsgi.py
+++ apiproject/apiproject/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+from django.core.wsgi import get_wsgi_application
+from os.path import join,dirname,abspath
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apiproject.settings")
-
 application = get_wsgi_application()
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apiproject.settings")
+sys.path.insert(0,PROJECT_DIR)
